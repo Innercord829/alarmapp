888bf84116f7f51bfa7653584b2eee6b5319e589
Innercord829 <adkinsdean3@gmail.com>
2025-02-27 01:38:46 -0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   void setAlarmTest(final alarmSettings) async {
     print("Alarm Set");
+    //test
     await Alarm.set(alarmSettings: alarmSettings);
   }
 
